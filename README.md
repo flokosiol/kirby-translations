e2aa0e02598afb2eba5a72c0a144c18d83abf29b
flokosiol <git@flokosiol.de>
2016-08-27 06:57:11 +0200

    transformation to plugin and delete init

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Kirby Translations
 
-![Version](https://img.shields.io/badge/version-0.2-green.svg)
+![Version](https://img.shields.io/badge/version-0.3-green.svg)
 
-With this field plugin for [Kirby 2](http://getkirby.com) you can display the translation status for pages in the panel. At the moment there are 3 possible states:
+With this plugin for [Kirby 2](http://getkirby.com) you can display the translation status for pages in the panel. At the moment there are 3 possible states:
 
 + **RED**: The translated `.txt`-file does not exist
 + **YELLOW**: The translated `.txt`-file exists, but the content is not up to date (checkbox unchecked)
@@ -25,11 +25,11 @@ kirby plugin:install flokosiol/kirby-translations
 
 ### Copy & Paste
 
-Add (if necessary) a new `fields` folder to your `site` directory. Then copy the whole content of this repository in a new folder called `translations`. Your directory structure should now look like this:
+Add (if necessary) a new `plugins` folder to your `site` directory. Then copy the whole content of this repository in a new folder called `translations`. Your directory structure should now look like this:
 
 ```
 site/
-  fields/
+  plugins/
     translations/
       ...
 ```
@@ -40,7 +40,7 @@ It is possible to add this plugin as a Git submodule.
 
 ```
 $ cd your/project/root  
-$ git submodule add https://github.com/flokosiol/kirby-translations.git site/fields/translations
+$ git submodule add https://github.com/flokosiol/kirby-translations.git site/plugins/translations
 ```
 
 For more information, have a look at [Working with Git](https://getkirby.com/docs/cookbook/working-with-git) in the Kirby cookbook.
--- translations.php
+++ translations.php
@@ -1,100 +1,33 @@
 <?php
 
 /**
- * Translations field
+ * Translations plugin
  *
  * @package   Kirby CMS
  * @author    Flo Kosiol <git@flokosiol.de>
  * @link      http://flokosiol.de
- * @version   0.2
+ * @version   0.3
  */
 
-class translationsField extends CheckboxField {
+$kirby->set('field', 'translations', __DIR__ . DS . 'fields' . DS . 'translations');
 
-  public static $assets = array(
-    'css' => array(
-      'translations.css',
-    ),
-  );
+// Routes
 
-  // Field setup
-
-  public function text() {
-    return 'Translation is up to date';
-  }
-
-  public function readonly() {
-    return false;
-  }
-
-  // Helper
-
-  public function deleteTranslation($language) {
-    if ($language->code() != $this->page()->site()->defaultLanguage()->code()) {
-      $textfile = $this->page->textfile(NULL, $language->code());
-      f::remove($textfile);
-    }
-    return;
-  }
-
-  public function isTranslated($language) {
-    $inventory = $this->page()->inventory();
-    return isset($inventory['content'][$language->code()]) ? TRUE : FALSE;
-  }
-
-  public function isUpToDate($language) {
-    $name = $this->name();
-    return $this->page()->content($language->code())->$name()->value();
-  }
-
-  public function statusIcon($language) {
-    if ($this->isTranslated($language)) {
-      return 'check';
-    }
-    return 'times';
-  }
-
-  public function cssClasses($language) {
-    $classes = array();
-
-    if ($this->isTranslated($language)) {
-      $classes[] = 'translated';
-      
-      if ($this->isUpToDate($language)) {
-        $classes[] = 'uptodate';
+if (site()->user()) {
+  kirby()->routes(array(
+    array(
+      'pattern' => 'translations.delete/(:all)',
+      'action'  => function($uri) {
+        if (s::get('lang') == get('lang') ) {
+          $languageCode = esc(get('lang')); // maybe no need to escape
+          $page = page($uri);
+          $textfile = $page->textfile(NULL, $languageCode);
+          f::remove($textfile);
+          $back_url = '/panel/pages/' . $page->id() . '/edit';
+          go($back_url);
+        }
+        go(site()->errorPage());
       }
-    }
-    else {
-      $classes[] = 'untranslated';
-    }
-
-    if ($this->page()->site()->language() == $language) {
-      $classes[] = 'active';
-    }
-
-    if ($this->page()->site()->defaultLanguage()->code() == $language->code()) {
-      $classes[] = 'default';
-    }
-
-    return implode(' ', $classes);
-  }
-
-  // Content
-
-  public function content() {
-    $wrapper = new Brick('div');
-    $wrapper->addClass('field-translations-wrapper');
-
-    $html = tpl::load( __DIR__ . DS . 'template.php', $data = array(
-      'site' => $this->page()->site(),
-      'page' => $this->page(),
-      'field' => $this,
-    ));
-
-    $wrapper->append($html);
-    $wrapper->append($this->input());
-
-    return $wrapper;
-  }
-
+    ),
+  ));
 }
