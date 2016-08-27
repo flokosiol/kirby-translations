6cf3fe4dc6485ae7f57c28ceccc625fc4e7e7712
flokosiol <git@flokosiol.de>
2016-08-27 21:19:50 +0200

    fixes #4

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
   <ul class="cf">
     <?php foreach ($site->languages() as $language): ?>
       <li class="<?php e($field->isTranslated($language),'translated','untranslated'); ?><?php e($site->language() == $language, ' active') ?>">
-        <a href="<?php echo url('panel/pages/' . $page->uri() . '/edit?language=' . $language->code()) ?>">
+        <a href="?language=<?php echo $language->code() ?>">
           <?php echo str::upper($language->code()) ?>
           <i class="fa fa-<?php e($field->isTranslated($language),'check','times'); ?>"></i>
         </a>
