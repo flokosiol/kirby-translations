467f8a912cea87a49d9545cc0d77737cdc237149
flokosiol <git@flokosiol.de>
2016-08-31 22:43:30 +0200

    delete confirm

--- fields/translations/assets/js/translations.js
+++ fields/translations/assets/js/translations.js
@@ -1,7 +1,19 @@
 $(function(){
+  
   $(document).on('click','.translations-delete',function (e) {
-    a = $(this);
-    
+    $(this).next('.translations-delete-confirm').fadeIn();
+    e.preventDefault();
+    return false;
+  });
+
+  $(document).on('click','.translations-delete-cancel-btn',function (e) {
+    $(this).parent('.translations-delete-confirm').fadeOut();
+    e.preventDefault();
+    return false;
+  });
+  
+  $(document).on('click','.translations-delete-confirm-btn',function (e) {
+    a = $(this);  
     url = a.data('url');
     id = a.data('id');
     language = a.data('language');
--- fields/translations/template.php
+++ fields/translations/template.php
@@ -4,10 +4,17 @@
       <a id="language-tab-<?php echo $language->code() ?>" class="language-tab" href="?language=<?php echo $language->code() ?>">
         <?php echo str::upper($language->code()) ?>
         <i class="fa fa-<?php echo $field->statusIcon($language); ?>"></i>
-      </a>
-      <a class="translations-delete" href="#" data-url="<?php echo url('translations.ajax.delete/') ?>" data-id="<?php echo $page->id() ?>" data-language="<?php echo $language->code() ?>">
-        <i class="delete fa fa-trash"></i>
-      </a>
+      </as>
+      <span class="delete">
+        <a class="translations-delete" href="#">
+          <i class="fa fa-trash"></i>
+        </a>
+        <span class="translations-delete-confirm">
+          <a class="translations-delete-confirm-btn btn btn-rounded btn-submit btn-negative" href="#" data-url="<?php echo url('translations.ajax.delete/') ?>" data-id="<?php echo $page->id() ?>" data-language="<?php echo $language->code() ?>">Delete <?php echo str::upper($language->code()) ?></a>
+          <a class="translations-delete-cancel-btn btn btn-rounded btn-submit" href="#">Cancel</a>
+          
+        </span>
+      </span>
     </li>
   <?php endforeach ?>
 </ul>
