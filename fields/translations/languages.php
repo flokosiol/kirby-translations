345f3888222b7458041e5a2a4e4b5fc9a7a2e7d9
Flo Kosiol <git@flokosiol.de>
2022-02-11 22:41:58 +0100

    translations for fr + nl #21

--- fields/translations/languages.php
+++ fields/translations/languages.php
@@ -17,4 +17,20 @@ return [
     'sync' => 'Synchronisiere',
     'with' => 'mit',
   ],
+  'fr' => [
+    'lbl_is_up_to_date' => 'La traductions est à jour',
+    'btn_cancel' => 'Annuler',
+    'btn_delete' => 'Supprimer',
+    'alert_update' => 'Cela réinitialisera le fichier. Tout changement sera perdu.',
+    'sync' => 'Syncroniser',
+    'with' => 'avec',
+   ],
+  'nl' => [
+    'lbl_is_up_to_date' => 'Deze vertaling is bijgewerkt',
+    'btn_cancel' => 'Annuleren',
+    'btn_delete' => 'Verwijderen',
+    'alert_update' => 'Dit zal het bestand resetten. Veranderingen worden dan gewist.',
+    'sync' => 'Synchroniseren',
+    'with' => 'met',
+   ],
 ];
